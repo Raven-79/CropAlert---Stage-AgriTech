02afaffaf7ea9ba967e2442440eca25cd46d7d03
Raven-79 <fatimaezzahraediraa@gmail.com>
2025-06-21 21:02:12 +0100

    Add configuration, extensions, and dependencies for Flask application

---
+++ server/app/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost/dbname')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY' ,'default_jwt_secret_key')
